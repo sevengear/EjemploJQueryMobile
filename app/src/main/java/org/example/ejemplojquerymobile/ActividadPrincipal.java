d013b8f35fe77592f772b2813e97b872326150d2
Miguel A. Nuñez <miguel.nunez.casado@gmail.com>
2018-03-23 19:16:27 +0100

    Terminado

--- app/src/main/java/org/example/ejemplojquerymobile/ActividadPrincipal.java
+++ app/src/main/java/org/example/ejemplojquerymobile/ActividadPrincipal.java
@@ -8,30 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class ActividadPrincipal extends AppCompatActivity {
+
+    WebView navegador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        navegador = (WebView) findViewById(R.id.webkit);
+        navegador.getSettings().setJavaScriptEnabled(true);
+        navegador.getSettings().setBuiltInZoomControls(false);
+        navegador.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_actividad_principal, menu);
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
         return true;
     }
 
